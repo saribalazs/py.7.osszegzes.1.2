f9825c4fb78ecd4f6868b30d5e0e20f0697ec37c
saribalazs <71698313+saribalazs@users.noreply.github.com>
2022-01-18 12:12:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+1.2 Feladat
+Készíts egy programot, amely [1;10] intervallumon generál 5 darab véletlen egész számot, és ezeket tárolja el egy listában! A program adja össze a lista elemeit, írja ki a képernyőre az összegüket és a lista elemeit!
+
+A program csak a páros számokat adja össze!
+'''
+import random
+
+lista = [random.randint(1, 10) for _ in range(5)]
+
+osszegzes = 0
+for szam in lista:
+    if szam % 2 == 0:
+        osszegzes += szam
+
+print(osszegzes)
+print(lista)
